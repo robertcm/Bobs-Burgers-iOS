9d0b01b4bf57d455cf21d04a0755e518286d4c38
Bobby Murphy <robertcm@gmail.com>
2011-10-11 00:20:56 -0700

    welcome view

---
+++ BobsBurgers/MainDelegate.h
@@ -0,0 +1,14 @@
+//
+//  MainDelegate.h
+//  BobsBurgers
+//
+//  Created by Robert Murphy on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef BobsBurgers_MainDelegate_h
+#define BobsBurgers_MainDelegate_h
+
+
+
+#endif
---
+++ BobsBurgers/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  BobsBurgers
+//
+//  Created by Robert Murphy on 10/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
