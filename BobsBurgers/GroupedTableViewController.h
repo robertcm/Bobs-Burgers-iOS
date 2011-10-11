4ec94f526c70b85cdbd9983b0628078defdfcf94
Bobby Murphy <robertcm@gmail.com>
2011-10-11 08:35:06 -0700

    locations view

---
+++ BobsBurgers/GroupedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupedTableViewController.h
+//  BobsBurgers
+//
+//  Created by Robert Murphy on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupedTableViewController : UITableViewController
+
+@end
