17d552e56324801f61c7ca509ec948af26445ded
Bobby Murphy <robertcm@gmail.com>
2011-10-11 09:16:03 -0700

    menu view

---
+++ BobsBurgers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  BobsBurgers
+//
+//  Created by Robert Murphy on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
