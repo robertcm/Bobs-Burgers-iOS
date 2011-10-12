dbb7c2a024369d6685f1d05c305c7ebcc1cb460f
Bobby Murphy <robertcm@gmail.com>
2011-10-11 21:43:39 -0700

    edit/delete item

---
+++ BobsBurgers/AsyncImageView.m
@@ -0,0 +1,23 @@
+//
+//  AsyncImageView.m
+//  BobsBurgers
+//
+//  Created by Robert Murphy on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AsyncImageView.h"
+
+@implementation AsyncImageView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
