574f7cd0b64c3147227528b0eae157a4fa0f9bc9
Bobby Murphy <robertcm@gmail.com>
2011-10-11 18:18:58 -0700

    add new item

---
+++ BobsBurgers/NewItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewItemViewController.h
+//  BobsBurgers
+//
+//  Created by Robert Murphy on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewItemViewController : UIViewController
+
+@end
